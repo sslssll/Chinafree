8077ec7133d41f007b3506d8c1f49e586db13442
sarah <1831027552@qq.com>
2021-11-16 14:29:25 +0800

    Initial commit

---
+++ my1/Chinalance/src/main/java/com/chinafree/china_free_lance/ChinalanceApplication.java
@@ -0,0 +1,13 @@
+package com.chinafree.china_free_lance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinalanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChinalanceApplication.class, args);
+    }
+
+}
---
+++ my1/Chinalance/src/test/java/com/chinafree/china_free_lance/ChinalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinafree.china_free_lance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinalanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
