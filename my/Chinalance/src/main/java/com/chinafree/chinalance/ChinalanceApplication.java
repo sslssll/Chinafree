70194ec24097c2a3d1e19272a9cfa4f56e37d238
saila.shen <1831027552@qq.com>
2021-11-23 09:41:47 +0800

    git

---
+++ my/Chinalance/src/main/java/com/chinafree/chinalance/ChinalanceApplication.java
@@ -0,0 +1,13 @@
+package com.chinafree.chinalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinalanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChinalanceApplication.class, args);
+    }
+
+}
